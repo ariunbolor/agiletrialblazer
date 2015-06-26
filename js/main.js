6360dafa2332c0d3f2ff7b3e7c9ac94b5f6c89b0
Bolorerdene Bundgaa <bolor@ariunbolor.org>
2015-06-26 12:33:12 -0400

    4 hour 43 min

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function atb() {
+
+(function () {
+   'use strict';
+
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
+        }
+    });
+
+}());
+
+}
+atb();
